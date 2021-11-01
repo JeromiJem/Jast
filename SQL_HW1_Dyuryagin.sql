6e74471f651fd7f1a133e0574cc83e2b1f66bb72
JeromiJem <antoha_1992@list.ru>
2021-11-01 15:29:36 +0300

    SQL_HW1_Dyuryagin.sql

--- SQL_HW1_Dyuryagin.sql
+++ SQL_HW1_Dyuryagin.sql
@@ -4,16 +4,19 @@ select * from students;
 --2. Вывести всех студентов в таблице
 select * from students;
 
+--3. Вывести только Id пользователей
+select id from students;
+
 --4. Вывести только имя пользователей
 select name from students;
 
---5. Вывести только email пользователей
+--5. Вывести только имя пользователей
 select email fron students;
 
---6. Вывести имя и email пользователей
+--6. Вывести только email пользователей
 select name, email from students;
 
---7. Вывести id, имя, email и дату создания пользователей
+--7. Вывести имя и email пользователей
 select id, name, email, created_on from students;
 
 --8. Вывести пользователей где password 12333
@@ -23,13 +26,13 @@ select * from students where password = '12333';
 select * from students where created_on = '2021-03-26 00:00:00';
 
 --10. Вывести пользователей где в имени есть слово Анна
-select * from students where name like '%Анна%';
+select * from students where name like '%Aiia%';
 
 --11. Вывести пользователей где в имени в конце есть 8
 select * from students where name like '%8';
 
---12. Вывести пользователей где в имени в есть буква а
-select * from students where name like '%а%' or name like 'а%' or name like '%а';
+--12.Вывести пользователей где в имени в есть буква а
+select * from students where name like '%a%' or name like 'a%' or name like '%a';
 
 --13. Вывести пользователей которые были созданы 2021-07-12 00:00:00
 select * from students where created_on = '2021-07-12 00:00:00';
@@ -76,10 +79,10 @@ select * from students where password = '12333' or password = '1m313' or passwor
 --27. Вывести пользователей где created_on равен 2020-10-03 00:00:00, 2021-05-19 00:00:00, 2021-03-26 00:00:00
 select * from students where created_on ='2020-10-03 00:00:00' or created_on = '2021-05-19 00:00:00' or created_on = '2021-03-26 00:00:00';
 
---28. Вывести минимальный id 
+--28. Вывести минимальный id  
 select MIN(id) from students;
 
---29. Вывести максимальный.
+--29. Вывести максимальный id
 select MAX(id) from students;
 
 --30. Вывести количество пользователей
